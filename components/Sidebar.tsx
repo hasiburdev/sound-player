461c34968398c4eae03f4136e17503a86c068c1b
Hasibur Rahman <90032528+hasiburdev@users.noreply.github.com>
2023-02-08 00:03:30 +0600

    Add sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Box,
+  List,
+  ListIcon,
+  ListItem,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+} from "react-icons/md";
+
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "/search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "/library",
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="120px" marginBottom="20px" paddingY="20px">
+          <Center>
+            <Image src="/logo.svg" alt="" width={120} height={60} />
+          </Center>
+        </Box>
+        <Box marginBottom="20px">
+          <List spacing={2}>
+            {navMenu.map((menuItem) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menuItem.name}>
+                <LinkBox>
+                  <Link href={menuItem.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menuItem.icon}
+                        color="white"
+                        marginRight="20px"
+                      />{" "}
+                      {menuItem.name}
+                    </LinkOverlay>
+                  </Link>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
